7d11182ed2c872421678b3c3cb12d97c50987434
YSH-NYK <yashnaik7664@gmail.com>
2024-08-07 16:47:18 +0530

    sidebar3

--- src/components/userSidebar.tsx
+++ src/components/userSidebar.tsx
@@ -24,7 +24,7 @@ import Link from "next/link";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
-
+import HomeSection from "@/pages/HomeSection"
 export function UserSidebar() {
   const links = [
     {
@@ -35,10 +35,10 @@ export function UserSidebar() {
       ),
     },
     {
-      label: "Post",
+      label: "Create",
       href: "#",
       icon: (
-        <IconPhotoPlus className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+        <IconPlus className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
       ),
     },
     {
@@ -87,7 +87,7 @@ export function UserSidebar() {
           </div>
         </SidebarBody>
       </Sidebar>
-      <Dashboard />
+      <HomeSection/>
     </div>
   );
 }
@@ -121,27 +121,5 @@ export const LogoIcon = () => {
   );
 };
 
-const Dashboard = () => {
-  return (
-    <div className="flex flex-1">
-      <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
-        <div className="flex gap-2">
-          {[...new Array(4)].map((_, i) => (
-            <div
-              key={"first" + i}
-              className="h-20 w-full rounded-lg bg-gray-100 dark:bg-neutral-800 animate-pulse"
-            ></div>
-          ))}
-        </div>
-        <div className="flex gap-2 flex-1">
-          {[...new Array(2)].map((_, i) => (
-            <div
-              key={"second" + i}
-              className="h-full w-full rounded-lg bg-gray-100 dark:bg-neutral-800 animate-pulse"
-            ></div>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
-};
+
+
